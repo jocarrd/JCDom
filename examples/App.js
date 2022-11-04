d880dcd0042b471767fdc3df51eb96b44ebe9a3d
jocarrd <jorgecarrera98d@gmail.com>
2022-11-04 09:29:29 +0100

    feat: add Event Handler to Custom Component

--- examples/App.js
+++ examples/App.js
@@ -4,7 +4,7 @@ import { JCDom } from "../src";
 export function App({ text }) {
   return (
     <div>
-      <button onClick={() => console.log("Prueba de Event Handler")}>
+      <button onClick={() => console.log("Prueba Event Handler")}>
         {text}
       </button>
     </div>
